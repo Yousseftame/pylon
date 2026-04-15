13c19124722f201b0bcfda50b50525b6f0c46394
Yousseftame <tamerosama73@gmail.com>
2026-04-15 11:17:49 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,6 +9,11 @@ export default defineConfig({
   plugins: [react(),
         tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
    server: {
     host: true,   // مهم جداً
     port: 5173
